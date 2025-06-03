1dc7141e1d11598bff0dc245d04ba1dee5efeb0e
anrzeoui <anar.urnukhbayar@gmail.com>
2025-06-03 12:34:47 +0800

    Vehicle

---
+++ MainVehicle.java
@@ -0,0 +1,94 @@
+public class MainVehicle {
+    public static void main(String[] args) {
+        Car car = new Car("Suzuki", "Swift", 2015, 15.1);
+        car.displayDetails(); 
+
+        Truck truck = new Truck("Ford", "F-150", 2016, 3.5);
+        truck.displayDetails(); 
+    }
+}
+
+class Vehicle {
+    private String make;
+    private String model;
+    private int year;
+
+    public Vehicle(String make, String model, int year) {
+        this.make = make;
+        this.model = model;
+        this.year = year;
+    }
+
+    public void displayDetails() {
+        System.out.println("Vehicle Details:");
+        System.out.println("Make: " + make);
+        System.out.println("Model: " + model);
+        System.out.println("Year: " + year);
+    }
+
+    public String getMake() {
+        return make;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public int getYear() {
+        return year;
+    }
+}
+
+class Car extends Vehicle {
+    private double trunkSize;
+
+    public Car(String make, String model, int year, double trunkSize) {
+        super(make, model, year);
+        this.trunkSize = trunkSize;
+    }
+
+    @Override
+    public void displayDetails() {
+        super.displayDetails();
+        System.out.println("Trunk Size: " + trunkSize + " cubic feet");
+    }
+
+    public double getTrunkSize() {
+        return trunkSize;
+    }
+
+    public void setTrunkSize(double trunkSize) {
+        if (trunkSize > 0) {
+            this.trunkSize = trunkSize;
+        } else {
+            System.out.println("Trunk size must be positive.");
+        }
+    }
+}
+
+class Truck extends Vehicle {
+    private double payloadCapacity;
+
+    public Truck(String make, String model, int year, double payloadCapacity) {
+        super(make, model, year);
+        this.payloadCapacity = payloadCapacity;
+    }
+
+    @Override
+    public void displayDetails() {
+        super.displayDetails();
+        System.out.println("Payload Capacity: " + payloadCapacity + " tons");
+    }
+
+    public double getPayloadCapacity() {
+        return payloadCapacity;
+    }
+
+    public void setPayloadCapacity(double payloadCapacity) {
+        if (payloadCapacity > 0) {
+            this.payloadCapacity = payloadCapacity;
+        } else {
+            System.out.println("Payload capacity must be positive.");
+        }
+    }
+}
